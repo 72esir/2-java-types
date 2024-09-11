c161eb154e12eab9365971802dbada0057b8c5ac
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-11 16:34:13 +0500

    Salamatin Aleksey.Task7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -4,18 +4,20 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long vertical = (long)(n / k) * k;
+        long horizontal = (long)(m / k) * k;
+        int count = 0;
 
-        return 0;
+        count = (int) (vertical * horizontal / (k*k));
+
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(10, 20, 5);
         System.out.println(result);
-        */
+
     }
 
 }
