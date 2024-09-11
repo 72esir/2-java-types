1cbe855220fbc96e419c761c659c21c8ac26e1d6
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-11 16:33:56 +0500

    Salamatin Aleksey.Task5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,37 @@
 package com.example.task05;
 
+import javafx.scene.control.TableRow;
+
 public class Task05 {
 
     public static String solution(int x) {
+        int flag = 1;
+        String res = "";
+        while (x > 0) {
+            if ((x % 10) % 2 == 0) {
+                x = x / 10;
+            }
+            else{
+                flag = 0;
+                x = x/10;
+            }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "FALSE";
+
+        }
+        if (flag == 1)
+            res = "TRUE";
+        else
+            res = "FALSE";
+
+        return res;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
